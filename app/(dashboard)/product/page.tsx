5decf375652bf6304b757106a48e987fe8bdad07
tsorak <karost0249@gmail.com>
2023-09-29 16:32:30 +0200

    Add product creator

--- app/(dashboard)/product/page.tsx
+++ app/(dashboard)/product/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import ProductCreator from "@/components/ProductCreator";
+
 export default function Products() {
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <main className="p-2">
+      <div className="max-w-xs p-2 border rounded-xl">
+        <p className="pb-1 mb-1 border-b opacity-80">Add a product</p>
+        <ProductCreator />
+      </div>
+    </main>
   );
 }
