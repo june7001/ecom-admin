638767722f1e53c4adc2e3dabc4d3671db769d85
June <june.zhao@yahoo.com>
2023-10-06 16:44:29 +0200

    remove files to start over

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { useRouter } from "next/navigation";
-// import { UserButton } from "@clerk/nextjs";
+
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  // Redirect to the dashboard
-  useRouter().replace("/overview");
   return (
     <div>
-      {/* <UserButton afterSignOutUrl="/" /> */}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
