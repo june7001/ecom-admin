4eedd578689d1e90519c9c854f598275887ad8d1
June <june.zhao@yahoo.com>
2023-10-09 15:22:25 +0200

    add toast and modal provider in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ToastProvider } from "@/providers/toast-provider";
+import { ModalProvider } from "@/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
