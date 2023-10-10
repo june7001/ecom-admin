61e7411c42c3409df97135fe4702802adaa52056
June <june.zhao@yahoo.com>
2023-10-10 19:42:21 +0200

    update nav routes

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -28,14 +28,9 @@ export function MainNav({
       active: pathname === `/${params.storeId}/categories`,
     },
     {
-      href: `/${params.storeId}/sizes`,
-      label: "Sizes",
-      active: pathname === `/${params.storeId}/sizes`,
-    },
-    {
-      href: `/${params.storeId}/colors`,
-      label: "Colors",
-      active: pathname === `/${params.storeId}/colors`,
+      href: `/${params.storeId}/brandnames`,
+      label: "Brandnames",
+      active: pathname === `/${params.storeId}/brandnames`,
     },
     {
       href: `/${params.storeId}/products`,
