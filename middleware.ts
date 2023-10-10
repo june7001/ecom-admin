111574bb0ccdb783dace06e5daeaff10e476b1c2
June <june.zhao@yahoo.com>
2023-10-10 10:05:42 +0200

    reverse

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path", "/store/*"],
+  publicRoutes: ["/api/:path*"],
 });
 
 export const config = {
-  matcher: ["/((?!...|_next).)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
