9da775aae7e6c5879dab970a49d93b8a2909f6d5
HarenIO <78878873+HarenIO@users.noreply.github.com>
2023-10-11 15:42:07 +0200

    move category fetch from table into parent page

--- app/(dashboard)/[storeId]/categories/page.tsx
+++ app/(dashboard)/[storeId]/categories/page.tsx
@@ -1,16 +1,43 @@
 "use client";
-import CategoryCreator from '@/components/CategoryCreator';
+import CategoryCreator from "@/components/CategoryCreator";
+import CategoryTable from "@/components/CategoryTable";
 import { useParams } from "next/navigation";
+import { useState, useEffect } from "react";
+
+type Category = {
+  id: string;
+  name: string;
+  storeId: string;
+  createdAt: string;
+};
 
 const Categories = () => {
-    const params = useParams();
-    const storeId = params.storeId;
+
+  const params = useParams();
+  const storeId = params.storeId;
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `http://localhost:3000/api/stores/${storeId}/category?storeId=${storeId}`
+      );
+      const data: Category[] = await res.json();
+      setCategories(data);
+    } catch (error) {
+      console.error("Failed to fetch categories", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-        <CategoryCreator storeId={`${storeId}`}/>
+      <CategoryCreator storeId={`${storeId}`} refreshCategories={fetchData} />
+      <CategoryTable storeId={`${storeId}`} categories={categories} />
     </>
-    
   );
 };
 
