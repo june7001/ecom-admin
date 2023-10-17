d057e74c65914d80047028e4593d787ec62da454
June <june.zhao@yahoo.com>
2023-10-17 15:08:17 +0200

    Add dynamic ProductPage for product handling

---
+++ app/(dashboard)/[storeId]/products/[productId]/page.tsx
@@ -0,0 +1,44 @@
+import prismadb from "@/lib/prismadb";
+
+import { ProductForm } from "./components/product-form";
+
+const ProductPage = async ({
+  params,
+}: {
+  params: { productId: string; storeId: string };
+}) => {
+  const product = await prismadb.product.findUnique({
+    where: {
+      id: params.productId,
+    },
+    include: {
+      images: true,
+    },
+  });
+
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  const brandNames = await prismadb.brandName.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductForm
+          categories={categories}
+          brandNames={brandNames}
+          initialData={product}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
